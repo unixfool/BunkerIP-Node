d9a2de410c93bf44f2130e24bb8e7404000ea748
y2k <66226195+unixfool@users.noreply.github.com>
2023-12-07 14:30:48 +0100

    update

--- console.js
+++ console.js
@@ -1,2 +1,13 @@
+import express from ("express");
+const app = express();
 //This is just for console log
-console.log("Bunker-IP Console Log System");
+
+app.get("/", async (req, res) => {
+    try {
+        console.log("Bunker-IP Console Log System");
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.listen(3000);
