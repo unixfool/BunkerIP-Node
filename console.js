e5d61d9c4619a4c470ec1592d61abf8101f95040
y2k <66226195+unixfool@users.noreply.github.com>
2023-12-07 14:35:20 +0100

    Update dotenv
    Co-Authored-By: ARI3L <66218989+ARI3LUnet@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,4 +1,9 @@
 import express from ("express");
+import dotenv from ("dotenv");
+import { config } from ("dotenv").config();
+
+const PORT = process.env.PORT 
+
 const app = express();
 //This is just for console log
 
